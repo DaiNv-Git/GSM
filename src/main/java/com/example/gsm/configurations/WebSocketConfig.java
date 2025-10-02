998634836c984807ee2bc832c66010eeec385a09
partnerdainvabbank <partnerdainv@abbank.vn>
2025-10-02 13:55:56 +0700

    add andpoind record

--- src/main/java/com/example/gsm/configurations/WebSocketConfig.java
+++ src/main/java/com/example/gsm/configurations/WebSocketConfig.java
@@ -20,6 +20,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
         registry.addEndpoint("/ws-record").setAllowedOriginPatterns("*").withSockJS();
-
     }
 }
