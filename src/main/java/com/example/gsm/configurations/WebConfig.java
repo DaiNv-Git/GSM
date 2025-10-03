35230083c455a19ea0ff56665483dc92328daf89
partnerdainvabbank <partnerdainv@abbank.vn>
2025-10-03 11:25:13 +0700

    add all

---
+++ src/main/java/com/example/gsm/configurations/WebConfig.java
@@ -0,0 +1,13 @@
+package com.example.gsm.configurations;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/home/record/**")
+                .addResourceLocations("file:/home/record/");
+    }
+}
